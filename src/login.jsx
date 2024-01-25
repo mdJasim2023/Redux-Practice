e20d8b3c2dd0ccdde2f2602475451ccc39e89706
mdJasim2023 <jasim127md@gmail.com>
2024-01-25 11:59:19 +0530

    cxc

--- src/login.jsx
+++ src/login.jsx
@@ -1,24 +1,38 @@
 import React from "react";
 import { useFormik } from "formik";
-import * as Yup from 'yup'
+import * as  Yup from 'yup'
 
 function Login(){
+    let [std,setStd] = React.useState([])
     const studentform = useFormik({
         initialValues:{
-            firstname:'',
-            lastname:'',
-            age:'',
-            gender:''
+            fname:'',
+            lname:'',
+            age:''
         },
+
         validationSchema:Yup.object({
-            firstname:
-        })
-        
+            fname:Yup.string().required('jasim'),
+            lname:Yup.string().required('jasim'),
+            age:Yup.string().required('jasim')
+        }),
+
+        onSubmit:(values)=>{
+            setStd([...std,values])
+        }
     })
+
     return(
         <div>
-            <form>
-
+            <form onSubmit={studentform.handleSubmit}>
+                {console.log(studentform)}
+                <input type="text" name="fname" onChange={studentform.handleSubmit} onBlur={studentform.handleBlur}/><br /><br />
+                <div>{studentform.touched.fname && studentform.errors.fname}</div>
+                <input type="text" name="lname"onChange={studentform.handleSubmit} onBlur={studentform.handleBlur}/><br /><br />
+                <div>{studentform.touched.lname && studentform.errors.lname}</div>
+                <input type="text" name="age"onChange={studentform.handleSubmit} onBlur={studentform.handleBlur} /><br /><br />
+                <div>{studentform.touched.age && studentform.errors.age}</div>
+                <button type="submit">save</button>
             </form>
         </div>
     )
