1d746f61cf7416952c15a0b50f5237fe3ac84371
mdJasim2023 <jasim127md@gmail.com>
2024-01-27 16:33:43 +0530

    jasi

--- src/store/Reducers/todoReducer.js
+++ src/store/Reducers/todoReducer.js
@@ -1,32 +1,46 @@
 let initialvalue = {
-    todos:[
-        {
-            task:'JasiM',
-            isDone:false
-    }
-],
-    new:'',
-    isDone:false
+    todos:[],
+    new:{
+        task:'',
+        isDone:false
+    },
+    selectIndex:null,
+    isUpdate:false
 }
 
 let TodoReducer = (state=initialvalue,action)=>{
- 
+    if(action.type==='UpdateTodo'){
+        return {...state,new:{task:action.payload}}
+    }
     if(action.type==='ADDTODO'){
-        return {...state,todos:[...state.todos,state.new]}
+        return {...state,todos:[...state.todos,{...state.new}]}
     }
     if(action.type==='Delete'){
         var temp = [...state.todos]
         temp.splice(action.index,1)
         return {...state,todos:[...temp]}
     }
-    if(action.type==='UpdateTodo'){
-        return {...state,new:action.payload}
-    }
+   
     if(action.type==='Done'){
-        var temp = [...state.todos]
-        
-        .isDone=!(temp[action.index].isDone)
+        let temp1 = [...state.todos]
+        temp1[action.index].isDone=!temp1[action.index].isDone
+        return {...state,todos:[...temp1]}
     }
+    if(action.type==='Undo'){
+        let temp2 = [...state.todos]
+        temp2[action.index].isDone=!temp2[action.index].isDone
+        return {...state,todos:[...temp2]}
+    }
+    if(action.type==='Edit'){
+        return {...state,isUpdate:true,selectIndex:action.payload}
+    }
+    if(action.type==='Updatebtn'){
+        let temp = [...state.todos];
+        temp[state.selectIndex].task=state.new.task
+        return {...state,todos:[...temp],isUpdate:false, selectIndex:null}
+    }
+
+
     return  state
 }
 
--- src/todo.jsx
+++ src/todo.jsx
@@ -1,23 +1,44 @@
-import React from "react";
+import React, { useRef } from "react";
 import { connect } from "react-redux";
+import './App.css'
 
 const Todolist = (props)=>{
+    let inp =React.useRef()
+    const handleEdit = (a,ind)=>{
+        props.dispatch({type:'Edit',payload:ind})
+        inp.current.value=a.task
+    }
     console.log(props);
     return(
-        <div className="border m-2 border-warning">
-            <div className="border m-5 w-25 p-3 border-success">
+        <div className="border  border-warning w-50" id="cen">
+            <div className="border m-5 w- p-3 border-success">
                 <h1 className="text-danger">TodoList :</h1>
-                <input type="text" onKeyUp={(e)=>{props.dispatch({type:'UpdateTodo',payload:e.target.value})}}/>
-                <button onClick={()=>{props.dispatch({type:'ADDTODO'})}}>AddTask</button>
+                <input ref={inp} type="text" className="p-1 m-1" onKeyUp={(e)=>{props.dispatch({type:'UpdateTodo',payload:e.target.value})}}/>
+                {!props.Todolist.isUpdate && <button className="btn btn-success" onClick={()=>{props.dispatch({type:'ADDTODO'})}}>AddTask</button>}
+                {props.Todolist.isUpdate && <button className="btn btn-secondary" onClick={()=>{props.dispatch({type:'Updatebtn'})}}>Update</button>}
+            </div>
+            <div className="m-5">
+                <input type="radio" name='tasks' />&nbsp;&nbsp;
+                <label><b>ALL</b></label>&nbsp;&nbsp;
+                <input type="radio" name='tasks'/>&nbsp;&nbsp;
+                <label><b>COMPLETE</b></label>&nbsp;&nbsp;
+                <input type="radio" name='tasks'/>&nbsp;&nbsp;
+                <label><b>INCOMPLETE</b></label>&nbsp;&nbsp;
             </div>
             <ul className="border border-info p-2 m-5">
+            <h1 className="text-secondary p-2">Todos</h1>
+           
                 {
                     props.Todolist.todos.map((a,ind)=>{
-                        return <li className="p-2 m-3 border w-25 list-group-item d-flex justify-content-around">
-                            <b>{a}</b>
-                            <button className="btn btn-success" onClick={()=>{props.dispatch({type:'Done'})}}>Done</button>
-                            <button className="btn btn-danger" onClick={()=>{props.dispatch({type:'Delete'})}}>Delete</button>
+                        return (<>
+                        <li className="p-2 m-3 border w-50 list-group-item d-flex justify-content-around rounded">
+                            <b style={a.isDone?{textDecoration:'line-through',color:'red'}:{}}>{a.task}</b>
+                            <button className="btn btn-success" onClick={()=>{props.dispatch(a.isDone?{type:'Done',index:ind}:{type:'Done',index:ind})}} style={a.isDone?{backgroundColor:'orange'}:{backgroundColor:'green'}}>{a.isDone?'Undo':'Done'}</button>
+                            {a.isDone && <button className="btn btn-danger" onClick={()=>{props.dispatch({type:'Delete',index:ind})}}>Delete</button>}
+                            {!a.isDone && <button className="btn btn-warning" onClick={()=>{handleEdit(a,ind)}}>Edit</button>}
                             </li>
+                            </>
+                            )
                     })
                 }
             </ul>
